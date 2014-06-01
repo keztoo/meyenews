25be13c4745347504f4c70b25685b2ea64c7157a
Ken Smith <ksmith@revelsystems.com>
2014-06-01 15:35:01 -0400

    fix database create script to accept command line parameters

--- Database/create_database.sh
+++ Database/create_database.sh
@@ -1,9 +1,16 @@
+#
+# Usage:
+# ./create_database.sh db_user_name db_password database_name
+# for example, if I had a mysql user named joe with password
+# blow and I had just created a database named joes_meyenews
+# I would run ...
+# ./create_database.sh joe blow joes_meyenews
+#
+mysql -u$1 -p$2 $3 < category.sql
+mysql -u$1 -p$2 $3 < data_source.sql
+mysql -u$1 -p$2 $3 < data_source_type.sql
+mysql -u$1 -p$2 $3 < user.sql
+mysql -u$1 -p$2 $3 < user_prefs.sql
+mysql -u$1 -p$2 $3 < session.sql
+mysql -u$1 -p$2 $3 < join_category_and_data_source.sql
 
-# TODO pass in creds and maybe even the mysql executable with path
-mysql -uken -pP1ucKWzZ ken_meyenews < category.sql
-mysql -uken -pP1ucKWzZ ken_meyenews < data_source.sql
-mysql -uken -pP1ucKWzZ ken_meyenews < data_source_type.sql
-mysql -uken -pP1ucKWzZ ken_meyenews < user.sql
-mysql -uken -pP1ucKWzZ ken_meyenews < user_prefs.sql
-mysql -uken -pP1ucKWzZ ken_meyenews < session.sql
-mysql -uken -pP1ucKWzZ ken_meyenews < join_category_and_data_source.sql
