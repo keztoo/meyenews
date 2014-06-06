bf3f8652f568f120457fba6a0b2edf8e765d6f89
Ken Smith <ksmith@revelsystems.com>
2014-06-05 20:22:09 -0400

    adding in .sh file that can be run as a cron to make things easier. note any hard coded paths and such should go in this file

---
+++ Server/CronJobs/ProcessIndex/ProcessIndexCron.sh
@@ -0,0 +1,2 @@
+
+python /home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/ProcessIndex.py /home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/views/index.tmpl /home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/views/carousel_section.tmpl /home/ken/sites/meyenews/index.html
