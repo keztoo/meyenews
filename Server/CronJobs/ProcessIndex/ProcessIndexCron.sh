6830ddf87be8d66696feac8666c48dd9494fbf68
Ken Smith <keztoo@yahoo.com>
2014-06-05 20:35:53 -0400

    final mods to make a cronable shell script which can be run every 5 minutes or so

--- Server/CronJobs/ProcessIndex/ProcessIndexCron.sh
+++ Server/CronJobs/ProcessIndex/ProcessIndexCron.sh
@@ -1,2 +1,9 @@
 
+#PYTHONPATH="${PYTHONPATH}://home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/HeadlineGrabbers/:/path/to/another/cool/python/package/"
+
+PYTHONPATH="${PYTHONPATH}://home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/HeadlineGrabbers/
+
+export PYTHONPATH
+
 python /home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/ProcessIndex.py /home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/views/index.tmpl /home/ken/code_line/meyenews/Server/CronJobs/ProcessIndex/views/carousel_section.tmpl /home/ken/sites/meyenews/index.html
+
